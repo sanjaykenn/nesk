bca8b28fdbcfb71f9672385d89d372c56f4b9dbd
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-16 16:59:59 +0200

    Adding ppu registers

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,4 +1,5 @@
 mod utils;
+mod registers;
 
 pub struct PPU {
 
---
+++ src/ppu/registers.rs
@@ -0,0 +1,116 @@
+use crate::{bit_field, bit_range};
+
+pub struct Registers {
+    pub control: Control,
+    pub mask: Mask,
+    pub status: Status,
+    pub oam_address: u8,
+    pub fine_x: u8,
+    pub vram_address: VRAMAddress
+}
+
+impl Registers {
+    pub fn new() -> Self {
+        Self {
+            control: Control::new(),
+            mask: Mask::new(),
+            status: Status::new(),
+            oam_address: 0,
+            fine_x: 0,
+            vram_address: VRAMAddress::new()
+        }
+    }
+}
+
+pub struct Control(u8);
+
+impl Control {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn set(&mut self, value: u8) {
+        self.0 = value
+    }
+
+    bit_field!(get: get_nametable_x @ 0);
+    bit_field!(get: get_nametable_y @ 1);
+    bit_field!(get: get_vram_increment @ 2);
+    bit_field!(get: get_sprite_pattern_table @ 3);
+    bit_field!(get: get_background_pattern_table @ 4);
+    bit_field!(get: get_sprite_size @ 5);
+    bit_field!(get: get_generate_nmi @ 7);
+}
+
+pub struct Mask(u8);
+
+impl Mask {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn set(&mut self, value: u8) {
+        self.0 = value
+    }
+
+    pub fn is_rendering_enabled(&self) -> bool {
+        self.get_show_sprites() || self.get_show_background()
+    }
+
+    bit_field!(get: get_grayscale @ 0);
+    bit_field!(get: get_show_background_leftmost_pixels @ 1);
+    bit_field!(get: get_show_sprites_leftmost_pixels @ 2);
+    bit_field!(get: get_show_background @ 3);
+    bit_field!(get: get_show_sprites @ 4);
+    bit_field!(get: get_emphasize_red @ 5);
+    bit_field!(get: get_emphasize_green @ 6);
+    bit_field!(get: get_emphasize_blue @ 7);
+}
+
+pub struct Status(u8);
+
+impl Status {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn get(&self) -> u8 {
+        self.0
+    }
+
+    bit_field!(set: set_sprite_overflow @ 5);
+    bit_field!(set: set_sprite_0_hit @ 6);
+    bit_field!(get_started_vertical_blank, set_started_vertical_blank @ 7);
+}
+
+pub struct VRAMAddress(u16);
+
+impl VRAMAddress {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn get(&self) -> u16 {
+        self.0
+    }
+
+    pub fn set(&mut self, value: u16) {
+        self.0 = value
+    }
+
+    pub fn get_attribute_index(&self) -> u16 {
+        self.get_attribute_y() << 3 | self.get_attribute_x()
+    }
+
+    bit_range!(get_tile_x, set_tile_x @ 0, 5 => u16);
+    bit_range!(get_tile_y, set_tile_y @ 5, 10 => u16);
+
+    bit_range!(get: get_attribute_x @ 2, 5 => u16);
+    bit_range!(get: get_attribute_y @ 7, 10 => u16);
+
+    bit_field!(get_nametable_x, set_nametable_x @ 10);
+    bit_field!(get_nametable_y, set_nametable_y @ 11);
+
+    bit_range!(get: get_nametable @ 10, 2 => u16);
+    bit_range!(get_fine_y, set_fine_y @ 12, 3 => u16);
+}
