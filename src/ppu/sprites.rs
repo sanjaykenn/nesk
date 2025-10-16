edf7646f3e127168f256da5c54ffe04b1d093480
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-16 18:47:52 +0200

    Adding ppu Sprite

--- src/ppu/sprites.rs
+++ src/ppu/sprites.rs
@@ -20,3 +20,57 @@ impl SpriteAttribute {
     bit_field!(get: get_flip_horizontal @ 6);
     bit_field!(get: get_flip_vertical @ 7);
 }
+
+pub struct Sprite {
+    y: u8,
+    id: u8,
+    attribute: SpriteAttribute,
+    x: u8
+}
+
+impl Sprite {
+    pub fn new() -> Self {
+        Self {
+            y: 0,
+            id: 0,
+            attribute: SpriteAttribute::new(),
+            x: 0,
+        }
+    }
+
+    pub fn get(&self, index: usize) -> u8 {
+        match index {
+            0 => self.y,
+            1 => self.id,
+            2 => self.attribute.get(),
+            3 => self.x,
+            _ => panic!("Sprite has only 4 attributes!")
+        }
+    }
+
+    pub fn set(&mut self, index: usize, value: u8) {
+        match index {
+            0 => self.y = value,
+            1 => self.id = value,
+            2 => self.attribute.set(value),
+            3 => self.x = value,
+            _ => panic!("Sprite has only 4 attributes!")
+        }
+    }
+
+    pub fn get_y(&self) -> u8 {
+        self.y
+    }
+
+    pub fn get_id(&self) -> u8 {
+        self.id
+    }
+
+    pub fn get_attribute(&self) -> &SpriteAttribute {
+        &self.attribute
+    }
+
+    pub fn get_x(&self) -> u8 {
+        self.x
+    }
+}
