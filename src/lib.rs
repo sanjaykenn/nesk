286041876e04a893a40d8299b17b92077b4748f8
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-16 19:55:37 +0200

    Adding Screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
 mod cpu;
 mod ppu;
+
+pub const WIDTH: usize = 256;
+pub const HEIGHT: usize = 240;
+pub const PIXEL_SIZE: usize = 3;
+
+pub trait Screen {
+    fn render(&mut self, pixels: &[[[u8; PIXEL_SIZE]; WIDTH]; HEIGHT]);
+}
