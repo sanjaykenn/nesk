e8f8eafe3fa9851dbc7f650418b1d321ddbe4f7a
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-16 17:43:50 +0200

    Moving memory implementation to mod.rs

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -3,7 +3,6 @@ use crate::ppu::registers::Registers;
 mod utils;
 mod registers;
 mod background;
-mod memory;
 
 pub struct PPU {
 
@@ -13,4 +12,20 @@ pub trait PPUMemory {
     fn read(&mut self, address: u16) -> u8;
     fn write(&mut self, address: u16, value: u8);
     fn get_registers(&mut self) -> &mut Registers;
+
+    fn read_nametable(&mut self, address: u16) -> u8 {
+        self.read(0x2000 | address & 0x0FFF)
+    }
+
+    fn read_attribute_table(&mut self, nametable: u16, address: u16) -> u8 {
+        self.read(0x23C0 | nametable << 10 | address)
+    }
+
+    fn read_pattern_table_tile_low(&mut self, pattern_table: bool, tile: u16, y: u16) -> u8 {
+        self.read(if !pattern_table { 0 } else { 0x1000 } | tile << 4 | y)
+    }
+
+    fn read_pattern_table_tile_high(&mut self, pattern_table: bool, tile: u16, y: u16) -> u8 {
+        self.read(if !pattern_table { 0 } else { 0x1000 } | tile << 4 | y | 8)
+    }
 }
