1df6cf409325404f1526031bc2647c315e52fe69
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-16 17:01:38 +0200

    Adding registers to ppu memory

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,3 +1,5 @@
+use crate::ppu::registers::Registers;
+
 mod utils;
 mod registers;
 
@@ -8,4 +10,5 @@ pub struct PPU {
 pub trait PPUMemory {
     fn read(&mut self, address: u16) -> u8;
     fn write(&mut self, address: u16, value: u8);
+    fn get_registers(&mut self) -> &mut Registers;
 }
