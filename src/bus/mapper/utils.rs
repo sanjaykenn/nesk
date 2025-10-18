a1cd497768bcc31dd55efd4b80931c408a6d1b60
Sanjay Kenn <sanjaykenn@gmail.com>
2025-10-18 02:05:22 +0200

    Fixing namespace mirroring

--- src/bus/mapper/utils.rs
+++ src/bus/mapper/utils.rs
@@ -1,14 +1,15 @@
 pub fn mirror_namespace_horizontal(address: u16) -> u16 {
-    address & 0x3FF | if address & 0x800 != 0 { 0x400 } else { 0x000 }
+    let mask = if address & 0x800 == 0 { 0x2000 } else { 0x2400 };
+    address & 0x3FF | mask
 }
 
 pub fn mirror_namespace_vertical(address: u16) -> u16 {
-    address & 0x7FF
+    address & ! 0x800
 }
 
 pub fn mirror_namespace(address: u16, horizontal: bool, vertical: bool) -> u16 {
     if horizontal & vertical {
-        address & 0x3FF
+        address & 0x3FF | 0x2000
     } else if horizontal {
         mirror_namespace_horizontal(address)
     } else {
